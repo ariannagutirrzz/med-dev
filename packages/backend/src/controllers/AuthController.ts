8452171b444fd40e64d8560917981386b3e72180
Madlekzz <italekzz@gmail.com>
2026-01-13 15:20:32 -0400

    Added validation for atleast 8 character long password

--- packages/backend/src/controllers/AuthController.ts
+++ packages/backend/src/controllers/AuthController.ts
@@ -13,6 +13,12 @@ export class AuthController {
           .json({ error: "Email, password, name and role are required" });
       }
 
+      if (password.length < 8) {
+        return res
+          .status(400)
+          .json({ error: "Password must be atleast 8 characters long." });
+      }
+
       // Check if user already exists
       const existingUser = await query(
         "SELECT id FROM users WHERE email = $1",
